28cce03de90d1fd46112f40e574fb7ecd88dfcd3
Kamran Mackey <kamranm1200@gmail.com>
2016-10-21 08:42:46 -0600

    Optimized the imports.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- EddieBot.py
+++ EddieBot.py
@@ -15,9 +15,8 @@
 # You should have received a copy of the GNU Lesser Public License
 # along with this program.  If not, see [http://www.gnu.org/licenses/].
 
-import logging
 import configparser
-
+import logging
 import os
 
 from telegram.ext import Updater
