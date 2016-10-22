32c79d84d51259116e25769518c4297908c682b1
Kamran Mackey <kamranm1200@gmail.com>
2016-10-21 20:37:32 -0600

    Removed unused import.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- EddieBot.py
+++ EddieBot.py
@@ -14,7 +14,6 @@
 #
 # You should have received a copy of the GNU Lesser Public License
 # along with this program.  If not, see [http://www.gnu.org/licenses/].
-import sys
 
 import configparser
 import logging
