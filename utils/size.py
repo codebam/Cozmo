ea6c612530dc78943d3afae7194be187668849ed
Kamran Mackey <kamranm1200@gmail.com>
2016-10-22 07:29:49 -0600

    Added "and contributors" to the rest of the files
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- utils/size.py
+++ utils/size.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2016 Kamran Mackey
+# Copyright (C) 2016 Kamran Mackey and contributors
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
