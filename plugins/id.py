47070d43f63e9a87a493cd8c9103e88d9e47e1b2
Barend <bvanrijn@users.noreply.github.com>
2016-10-23 21:00:38 +0200

    made id plugin and added myself as a contributor :)

--- plugins/id.py
+++ plugins/id.py
@@ -2,4 +2,5 @@ def id_plugin(bot, update):
     uname = update.message.from_user.first_name
     uid = update.message.from_user.id
 
-    bot.sendMessage(chat_id=update.message.chat_id, text="You are *{0}* with ID `{1}`.".format(uname, uid), reply_to_message_id=update.message.chat_id)
+    bot.sendMessage(chat_id=update.message.chat_id, text="You are *{0}* with ID `{1}`.".format(
+        uname, uid), parse_mode='Markdown')
