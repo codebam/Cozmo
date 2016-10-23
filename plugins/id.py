5e77e69ced7ae463420bfc20d52423a98ab965a0
Kamran Mackey <kamranm1200@gmail.com>
2016-10-23 17:23:24 -0600

    Migrated id plugin to use the new msg shortcut added in pytgbot 5.1.1
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- plugins/id.py
+++ plugins/id.py
@@ -1,6 +1,24 @@
-def id_plugin(bot, update):
+#
+# Copyright (C) 2016 Kamran Mackey and contributors
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see [http://www.gnu.org/licenses/].
+
+
+def id_plugin(_, update):
     uname = update.message.from_user.first_name
     uid = update.message.from_user.id
 
-    bot.sendMessage(chat_id=update.message.chat_id, text="You are *{0}* with ID `{1}`.".format(
-        uname, uid), reply_to_message_id=update.message.message_id, parse_mode='Markdown')
+    update.message.reply_text(parse_mode='Markdown',
+                              quote=True,
+                              text="You are *{0}* with ID `{1}`.".format(uname, uid))
