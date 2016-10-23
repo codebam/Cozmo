2d0f79fa8fdf219cb9314854d5634f1b4b0e4637
Barend <bvanrijn@users.noreply.github.com>
2016-10-23 21:04:52 +0200

    now replies to the user

--- plugins/id.py
+++ plugins/id.py
@@ -3,4 +3,4 @@ def id_plugin(bot, update):
     uid = update.message.from_user.id
 
     bot.sendMessage(chat_id=update.message.chat_id, text="You are *{0}* with ID `{1}`.".format(
-        uname, uid), parse_mode='Markdown')
+        uname, uid), reply_to_message_id=update.message.message_id, parse_mode='Markdown')
