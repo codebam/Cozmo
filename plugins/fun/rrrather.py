2826f20d456719e3c934b50c8c8e3cab18d7216f
Kamran Mackey <kamranm1200@gmail.com>
2016-10-27 04:13:18 -0600

    Moved tags back to its own variable.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- plugins/fun/rrrather.py
+++ plugins/fun/rrrather.py
@@ -34,6 +34,7 @@ def rrrather(_, update):
     choice_a = rrrather_json["choicea"]
     choice_b = rrrather_json["choiceb"].replace("?", "")
     votes = '{0:,}'.format(rrrather_json["votes"])
+    tags = rrrather_json["tags"].replace(",", ", ")
     link = rrrather_json["link"].replace("http", "https")
 
     # Message text
@@ -50,7 +51,7 @@ def rrrather(_, update):
                                                        choice_a,
                                                        choice_b,
                                                        votes,
-                                                       rrrather_json["tags"].replace(",", ", "),
+                                                       tags,
                                                        view_text,
                                                        link))
     except AttributeError:
