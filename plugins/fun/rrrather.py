e957135f6467b5999db676a202d2405b55db5530
Kamran Mackey <kamranm1200@gmail.com>
2016-10-27 03:34:42 -0600

    Move view question text to its own variable
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- plugins/fun/rrrather.py
+++ plugins/fun/rrrather.py
@@ -36,13 +36,17 @@ def rrrather(_, update):
     votes = rrrather_json["votes"]
     link = rrrather_json["link"].replace("http", "https")
 
+    # Responses
+    view_text = "*View question on rrrather*"
+
     # Message text
     update.message.reply_text(parse_mode='Markdown',
                               disable_web_page_preview=True,
                               text="{0}: {1} or {2}?\n\n"
                                    "*Votes*: {3}\n"
-                                   "*View question on rrrather*: {4}".format(title,
-                                                                             choice_a,
-                                                                             choice_b,
-                                                                             votes,
-                                                                             link))
+                                   "{4}: {5}".format(title,
+                                                     choice_a,
+                                                     choice_b,
+                                                     votes,
+                                                     view_text,
+                                                     link))
