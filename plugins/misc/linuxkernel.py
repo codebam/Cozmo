959c9abb37826d6fc7c675e3ca1b68345bc5824b
Kamran Mackey <kamranm1200@gmail.com>
2016-10-28 08:14:55 -0600

    Renamed a couple variables.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- plugins/misc/linuxkernel.py
+++ plugins/misc/linuxkernel.py
@@ -21,10 +21,10 @@ def kernel(_, update):
     rid of the "The latest version of the Linux
     Linux kernel is" crap as we don't need it.
     """
-    contents = requests.get("https://www.kernel.org/finger_banner").text
-    contents = re.sub(r"The latest(\s*)", '', contents)
-    contents = re.sub(r"version of the Linux kernel is:(\s*)", '- ', contents)
-    lines = contents.split("\n")
+    kernel_retrieval = requests.get("https://www.kernel.org/finger_banner").text
+    kernel_ver_regex_a = re.sub(r"The latest(\s*)", '', kernel_retrieval)
+    kernel_ver_regex_b = re.sub(r"version of the Linux kernel is:(\s*)", '- ', kernel_ver_regex_a)
+    lines = kernel_ver_regex_b.split("\n")
 
     message = "*Linux kernel versions*:\n\n" \
               "{}".format("\n".join(line for line in lines[:-1]))
