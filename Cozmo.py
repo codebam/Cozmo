3c775b301db8bc855e48b1a6e63153f1f8cfea50
Kamran Mackey <kamranm1200@gmail.com>
2016-10-28 03:26:38 -0600

    Simplified license headers and moved system.py to /util.
    Signed-off-by: Kamran Mackey <kamranm1200@gmail.com>

--- Cozmo.py
+++ Cozmo.py
@@ -6,14 +6,6 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see [http://www.gnu.org/licenses/].
 
 import logging as log
 import sys
@@ -59,7 +51,7 @@ def main():
             logger.info("Generating the config file.")
             config.write(configfile)
             logger.info("Generated the config file.")
-            exit(0) # This is the key to success
+            exit(0)  # Exit after the config file has generated.
     else:
         logger.info("Config file found. Not overwriting existing file.")
 
@@ -97,7 +89,7 @@ def main():
     from plugins.start import start
     dp.add_handler(CommandHandler('start', start))
 
-    from plugins.system import system
+    from plugins.util.system import system
     dp.add_handler(CommandHandler('system', system))
     logger.info("Plugin loading sequence complete. All plugins loaded.")
 
